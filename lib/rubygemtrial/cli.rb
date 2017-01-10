214729edcdfc5254012d369010f6f15c0b8e73e7
surajhell88 <surajhell88@gmail.com>
2017-01-10 23:23:16 +0530

    Added 4 more sub-commands to look more geeky.

--- lib/rubygemtrial/cli.rb
+++ lib/rubygemtrial/cli.rb
@@ -6,5 +6,21 @@ module Rubygemtrial
     def hello()
       puts "Hello World!"
     end
+    desc "setup", "This will ask for token"
+    def setup()
+      puts "I will ask you the token later!"
+    end
+    desc "reset", "This will forget you"
+    def reset()
+      puts "Aww I am missing you already!"
+    end
+    desc "submit", "This will submit your work"
+    def submit()
+      puts "Way to go!"
+    end
+    desc "test", "This will test you"
+    def test()
+      puts "Bomb in the hole!"
+    end
   end
 end
